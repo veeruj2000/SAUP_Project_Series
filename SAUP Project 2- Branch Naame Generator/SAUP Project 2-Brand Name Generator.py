46f87dd121f5cbd97742b0cab11a5917e76e499f
UJWAL PANKAJ JAIN <57255611+veeruj2000@users.noreply.github.com>
2022-03-03 22:39:40 +0530

    SAUP Project 2-Brand Name Generator

---
+++ SAUP Project 2- Branch Naame Generator/SAUP Project 2-Brand Name Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Brand Name Generator!")
+
+city = input("What City did you grow up in?: ")
+
+nick = input("What's your nickname name?: ")
+
+print("Your Brand Name could be " + city + " " + nick)
